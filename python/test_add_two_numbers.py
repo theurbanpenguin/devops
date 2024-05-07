5044fea5c219ae8871ac8b098c546040339bfe42
TheUrbanPenguin <andrew.mallett@theurbanpenguin.com>
2024-05-07 13:45:17 +0100

    add jenkins

--- python/test_add_two_numbers.py
+++ python/test_add_two_numbers.py
@@ -7,8 +7,6 @@ def test_add_two_positive_integers():
 def test_add_negative_and_positive_integer():
     assert add_two_numbers(-1, 1) == 0, "Should be 0"
 
-def test_add_two_floats():
-    assert add_two_numbers(1.1, 2.2) == 3.3, "Should be 3.3"
 
 def test_add_integer_and_float():
     assert add_two_numbers(5, 2.5) == 7.5, "Should be 7.5"
