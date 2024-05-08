f6b11ccde0c59bf003237d3fb99a6a8a5013cd20
TheUrbanPenguin <andrew.mallett@theurbanpenguin.com>
2024-05-08 09:11:53 +0100

    add repo script

---
+++ setup/repo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
